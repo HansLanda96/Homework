99eddc2256d4f03a7dbc721d319bb34be43a6c0c
Iegor <duuurrr17@gmail.com>
2022-06-16 02:27:21 +0300

    adding docstrings

--- homework_4/task_3.py
+++ homework_4/task_3.py
@@ -1,7 +1,9 @@
-from task_2 import input_number
+from task_2 import input_number as inp
+"""import module(input_number) from task_2"""
 
 
 def sequence_nums(first_num: int, second_num: int, var_list: list):
+    """create func that allows us to iterate and append list"""
     while first_num > second_num or first_num < second_num:
         if first_num < second_num:
             var_list.append(first_num)
@@ -13,12 +15,14 @@ def sequence_nums(first_num: int, second_num: int, var_list: list):
 
 
 def main():
+    """create main function with imported module from task_2"""
     var_list = []
-    first_num = input_number("Insert first num: ")
-    second_num = input_number("Insert second num: ")
+    first_num = inp("Insert first number: ")
+    second_num = inp("Insert second number: ")
     sequence_nums(first_num, second_num, var_list)
     print(f'Sequence of numbers equal : {var_list}')
 
 
 if __name__ == "__main__":
+    """enter point of program"""
     main()
