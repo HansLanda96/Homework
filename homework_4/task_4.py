18fadf1c71feaed1196471e18e8d35ff4479702e
Iegor <duuurrr17@gmail.com>
2022-06-16 02:28:11 +0300

    add file

---
+++ homework_4/task_4.py
@@ -0,0 +1,15 @@
+def ladder(start: int, end=9):
+    """"func allows us to print new string for each iteration"""
+    ladder_step = ''
+    for num in range(start, end + 1):
+        ladder_step += str(num)
+        print(ladder_step)
+
+
+def main():
+    ladder(1)
+
+
+if __name__ == "__main__":
+    """"enter point of program"""
+    main()
