20a86f69af1ebdf828fafd463efb22c00ddddc28
Iegor <duuurrr17@gmail.com>
2022-06-17 17:05:31 +0300

    polish

--- homework_4/task_5.py
+++ homework_4/task_5.py
@@ -33,13 +33,13 @@ And we will not have any probs with specific symbols or space bar
 In this case very funny to write message for example, that's give funny results:D"""
 
 row = str(input("Insert any elements in a row: "))
-print(f'\nYou created a row that includes this elements: {row}'
-      f'\nThird element in a row: {row[2]}'
-      f'\nPenultimate element in a row: {row[-2]}'
-      f'\nFirst five elements in a row: {row[0 : 5]}'
-      f'\nWhole row without two last elements: {row[0 : -2]}'
-      f'\nEvery "even" element in a row: {row[0 : : 2]}'
-      f'\nEvery "odd" element in a row: {row[1 : : 2]}'
-      f'\nReversed row: {row[ : : -1]}'
-      f'\nReversed row with every second element: {row [-1 : : -2  ]}'
-      f'\nLength of row: {len(row)}')
+# print(f'\nYou created a row that includes this elements: {row}'
+#       f'\nThird element in a row: {row[2]}'
+#       f'\nPenultimate element in a row: {row[-2]}'
+#       f'\nFirst five elements in a row: {row[0 : 5]}'
+#       f'\nWhole row without two last elements: {row[0 : -2]}'
+#       f'\nEvery "even" element in a row: {row[0 : : 2]}'
+#       f'\nEvery "odd" element in a row: {row[1 : : 2]}'
+#       f'\nReversed row: {row[ : : -1]}'
+#       f'\nReversed row with every second element: {row [-1 : : -2  ]}'
+#       f'\nLength of row: {len(row)}')
