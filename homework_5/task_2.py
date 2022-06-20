0177146dfd3312e7e4f87fa953709477c7a1cb4a
Iegor <duuurrr17@gmail.com>
2022-06-21 02:16:04 +0300

    polish

--- homework_5/task_2.py
+++ homework_5/task_2.py
@@ -1,10 +1,8 @@
 from string import punctuation
 """from built-in library importing punctuation"""
 
-
-def amount_words(words: str) -> int:
-    """Strip punctuation symbols in my string, so punctuation can't be count as 'word' """
-    return sum([words.strip(punctuation).isalpha() for words in words.split()])
+"""Strip punctuation symbols in my string, so punctuation can't be count as 'word' """
+def amount_words(words: str) -> int: return sum([words.strip(punctuation).isalpha() for words in words.split()])
 
 
 def amount_symbols(symbols: str) -> int: return len(symbols) - symbols.count(' ')
