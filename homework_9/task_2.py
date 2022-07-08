0c9db31e79c6b9bba6a246425e98b3ee0873e413
Iegor <duuurrr17@gmail.com>
2022-07-08 08:18:06 +0300

    add one more solution

--- homework_9/task_2.py
+++ homework_9/task_2.py
@@ -10,6 +10,30 @@ def sneaky_mail(email: str) -> str:
 
 if __name__ == '__main__':
     try:
-        print(sneaky_mail("VasyaVoron@gmail.com"))
+        print(sneaky_mail("VasyaVoron@hillel.com"))
     except ValueError:
         print("Enter the valid email. You miss '@' sign")
+
+
+# This func will count amount of letters and change them to "*"
+# Also func separate domain name and domain tail
+#
+#
+# def sneaky_mail(email: str) -> str:
+#     email = email[0:2] + "*" * (len(email) - 3) + email[-1]
+#     return email
+#
+#
+# def asterisk_mail(email: str) -> str:
+#     name, domain = email.split("@")
+#     dom_name, dom_tail = domain.split(".")
+#     name = sneaky_mail(name)
+#     dom_name = sneaky_mail(dom_name)
+#     return f'{name}@{dom_name}.{dom_tail}'
+#
+#
+# if __name__ == '__main__':
+#     try:
+#         print(asterisk_mail("VasyaVoron@hillel.com"))
+#     except ValueError:
+#         print("Enter the valid email. You miss '@' sign")
